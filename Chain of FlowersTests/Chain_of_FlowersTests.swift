ee78e745cd3441322c18bc2a8db9817fe0a816bf
Xuan Nguyen <darth.xuan@gmail.com>
2014-08-27 20:06:04 -0600

    Initial Commit

---
+++ Chain of FlowersTests/Chain_of_FlowersTests.swift
@@ -0,0 +1,36 @@
+//
+//  Chain_of_FlowersTests.swift
+//  Chain of FlowersTests
+//
+//  Created by Xuan Nguyen on 8/27/14.
+//  Copyright (c) 2014 Xuan Nguyen. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Chain_of_FlowersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
